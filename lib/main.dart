3f4ab379f3a88233ef46e4decaa76f8096d0c0fe
DavidGamFlo <davidgamflo@comunidad.unam.mx>
2021-09-28 21:41:19 -0500

    Version Final

--- lib/main.dart
+++ lib/main.dart
@@ -36,12 +36,14 @@ class ListaComidas extends StatelessWidget {
     required this.descripcion,
     required this.rated,
     required this.precio,
+    this.descripcionc = "",
   }) : super(key: key);
   final Widget imagen;
   final String titulo;
   final String descripcion;
   final double rated;
   final double precio;
+  final String descripcionc;
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +60,7 @@ class ListaComidas extends StatelessWidget {
             flex: 3,
             child: _DescripcionComidas(
               titulo: titulo,
-              descripcion: descripcion,
+              descripcion: descripcion.substring(0, 20) + "...",
               rated: rated,
               precio: precio,
             ),
@@ -229,7 +231,7 @@ class Comidas extends StatelessWidget {
           imagen: Container(
             decoration: BoxDecoration(
               image: const DecorationImage(
-                image: AssetImage('assets/Ayudante.jpg'),
+                image: AssetImage('assets/Suaperro.jpeg'),
                 fit: BoxFit.cover,
                 scale: 1.0,
               ),
@@ -249,7 +251,7 @@ class Comidas extends StatelessWidget {
           imagen: Container(
             decoration: BoxDecoration(
               image: const DecorationImage(
-                image: AssetImage("assets/descarga.jpg"),
+                image: AssetImage("assets/Pepitos.jpeg"),
                 fit: BoxFit.cover,
               ),
               border: Border.all(
@@ -262,20 +264,280 @@ class Comidas extends StatelessWidget {
           precio: 50.00,
           descripcion: 'Pepinguinos bien delinux',
         ),
+        ListaComidas(
+          titulo: 'Pozolito',
+          rated: 4.8,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Pozole.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 80.00,
+          descripcion: 'Rico pozole rojo con carne al gusto, tostadas y crema',
+        ),
+        ListaComidas(
+          titulo: 'Mole',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Mole.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 60.00,
+          descripcion: 'Rico mole oaxaqueño con pollo y enchiladas',
+        ),
+        ListaComidas(
+          titulo: 'Tacos dorados',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Tacos.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 50.00,
+          descripcion:
+              'Orden de 4 tacos dorados rellenos de pollo con aguacate crema y queso',
+        ),
+        ListaComidas(
+          titulo: 'Barbacha',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/barbacoa.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 20.00,
+          descripcion: 'Barbacoa de horno estilo hidalguense',
+        ),
+        ListaComidas(
+          titulo: 'Enchiladas Suizas',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/enchiladas.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 80.00,
+          descripcion:
+              'Deliciosas enchiladas suizas verdes o rojas con guarnicion y carne al gusto',
+        ),
+        ListaComidas(
+          titulo: 'Carnitas',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/carnitas.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 25.00,
+          descripcion:
+              'Deliciosos tacos de carnitas bien surtidos estilo Michoacán',
+        ),
+        ListaComidas(
+          titulo: 'Chiles en nogada',
+          rated: 5.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage('assets/Chiles.jpeg'),
+                fit: BoxFit.cover,
+                scale: 1.0,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 150.00,
+          descripcion: 'Deliciosos chiles en nogada',
+        ),
+        ListaComidas(
+          titulo: 'Tlayudas',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Tlayuda.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 45.00,
+          descripcion: 'Ricas tlayudas estilo Oaxaca',
+        ),
+        ListaComidas(
+          titulo: 'Agua Fresca',
+          rated: 4.8,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Aguas.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 30.00,
+          descripcion: 'Aguas frescas por litro de sabores de temporada',
+        ),
+        ListaComidas(
+          titulo: 'Quesadillas',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Quesadillas.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 20.00,
+          descripcion: 'Quesadillas de diferentes guisados con o sin queso',
+        ),
+        ListaComidas(
+          titulo: 'MichesTepiteñas',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Miches.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 75.00,
+          descripcion: 'Refrescantes miches al estilo tepiteño',
+        ),
+        ListaComidas(
+          titulo: 'Tamales',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Tamales.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 15.00,
+          descripcion: 'Tamales oaxaqueños tamales calientitos',
+        ),
+        ListaComidas(
+          titulo: 'Refrescos',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Refrescos.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 20.00,
+          descripcion: 'Refrescos (600 ml) embotellados',
+        ),
+        ListaComidas(
+          titulo: 'Pizza',
+          rated: 4.0,
+          imagen: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/Pizza.jpeg"),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          precio: 100.00,
+          descripcion: 'Pizza tamaño mediano de diferentes especialidades',
+        ),
       ],
     );
   }
 }
-
-class Todo {
-  final String titulo;
-  final String descripcion;
-  final double rated;
-  final Widget imagen;
-  Todo(this.titulo, this.descripcion, this.imagen, this.rated);
-}
-
-abstract class ListItem {}
 
 void main() {
   runApp(const MyApp());
